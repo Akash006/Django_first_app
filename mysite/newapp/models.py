8cdaa917749c9a7e353d53f500a07244f7555c13
Akash006 <mastermindvishu97@gmail.com>
2020-06-27 10:55:43 -0700

    Adding search and pagination feature

---
+++ mysite/newapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class MovieData(models.Model):
+
+    def __str__(self):
+        return self.name
+    
+    name = models.CharField(max_length=200)
+    rating = models.FloatField()
