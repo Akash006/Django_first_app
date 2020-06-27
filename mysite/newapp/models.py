56ff87b013e309e9fc1347032e0ec50dd2f5662a
Akash006 <mastermindvishu97@gmail.com>
2020-06-27 11:00:49 -0700

    Revert "Adding search and pagination feature"
    This reverts commit 8cdaa917749c9a7e353d53f500a07244f7555c13.

--- mysite/newapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class MovieData(models.Model):
-
-    def __str__(self):
-        return self.name
-    
-    name = models.CharField(max_length=200)
-    rating = models.FloatField()
